59b4434fc9ce99fc6552a7b087ecb8ffc7d00cc6
Keshon Royster <k.royster512@gmail.com>
2022-09-30 18:11:19 -0500

    Fixed syntax issues, added a condition and changed name for length

--- script.js
+++ script.js
@@ -6,20 +6,25 @@ var upperChars  = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var numberChars  = '0123456789';
 var specialChar  = '!@#$%^&*()';
 
+
+
+
 // Write password to the #password input
 function writePassword() {
-   generateBtn.addEventListener("click", function(){   userChoice = prompt ('How long would you like your password to be? Must be between 8 and 128 characters.');
-    if (userChoice < 8){
+   generateBtn.addEventListener("click", function(){   
+    userLength = prompt ('How long would you like your password to be? Must be between 8 and 128 characters.');
+    if (userLength < 8) {
       alert('Characters must be at least 8 characters long.')
-    if (userChoice > 128){
-      alert ('Characters cannot be longer than 128 characters.')
+    }if (userLength > 128) {
+        alert('Characters cannot be longer than 128 characters.')
+      }if (userLength = NaN){
+        alert('Must input a number.');
       }
-    };
-
   //var password = generatePassword();
  // var passwordText = document.querySelector("#password")
   //passwordText.value = password;
 });
+
 }
 // Add event listener to generate button
 
