1248b0f1530696571cc0de6cdb7ab7865549cc52
Keshon Royster <k.royster512@gmail.com>
2022-10-01 16:37:10 -0500

    added for loop. addition assignments

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Dynamic-Password-Generator
+# **Dynamic-Password-Generator**
+
+## **Description**
+
+This is my Dynamic Password Generator. The focus was to create a random password generator with JavaScript. When a user clicks the 'Generate Password' button, they are given a series of prompts for their password. Once complete, the new password will be displayed in the text area. 
+
+Github Repository - 
+
+Application Screenshot - ![Application screenshot](pwordscreen.png)
--- script.js
+++ script.js
@@ -7,6 +7,7 @@ var numbChar = '0123456789';
 var specialChar = '!@#$%^&*()_-+=';
 var userChoice = '';
 
+//Function to define generate password
 function generatePassword() {
      userLength = prompt('How long would you like your password to be? Must be between 8 and 128 characters.');
       if (userLength < 8 || userLength > 128 )  {
@@ -19,6 +20,7 @@ function generatePassword() {
       else if (!userLength) {
           return;
       }
+      //If user answers  the prompt correctly, the following code will run.
         else {
           var userLowerChar = confirm('Would you like to include lowercase characters?');
           if (userLowerChar) {
@@ -36,18 +38,20 @@ function generatePassword() {
           if (userSpecialChar) {
             userChoice += specialChar
             };
+            //If user does not give at least one parameter, they are stopped here and will need to start over.
           if (userLowerChar === false && userUpperChar === false && userNumbChar === false && userSpecialChar === false) {
             alert ('You must select at least one type of character');
           }
         }
+        // Variable to assign our new password. Left empty so it can be assigned after the for loop.
       var newPassword = '';
+      //Genertates a random password each time the previous condition are met and exectued.
         for (var i = 0; i < userLength; i++) {
           newPassword += userChoice.charAt(Math.floor(Math.random() * userChoice.length));
         }
+        //will display new password.
         return newPassword;
       }
-
-
   function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
